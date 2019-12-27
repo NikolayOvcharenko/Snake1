86275437cdd3d45d834ad580a59f3c16dd9ae757
Nikolay Ovcharenko <unicorn_nels@mail.ru>
2019-12-28 00:01:23 +0300

    в процессе 2

--- src/Snake/Snake.java
+++ src/Snake/Snake.java
@@ -5,10 +5,12 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.ArrayList;
 import java.util.Random;
 
-public class Snake implements ActionListener {
+public class Snake implements ActionListener, KeyListener {
 
     public JFrame jframe;
     public RenderPanel renderPanel;
@@ -19,7 +21,7 @@ public class Snake implements ActionListener {
 
     public ArrayList<Point> snakeParts = new ArrayList<>();
     public static final int UP = 0, DOWN = 1, LEFT = 2, RIGHT = 3, SCALE = 10;
-    public  int ticks = 0, direction = DOWN, score , tailLength;
+    public  int ticks = 0, direction = DOWN, score , tailLength = 10;
     public Point head, cherry;
     public Random random;
     public Dimension dim;
@@ -35,10 +37,16 @@ public class Snake implements ActionListener {
         jframe.add(renderPanel = new RenderPanel());
         jframe.setSize(800,600);
         jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jframe.addKeyListener(this);
         head = new Point(0,0);
         random = new Random();
         cherry = new Point(dim.width / SCALE,dim.height / SCALE);
+
+        for (int i = 0; i < tailLength ; i++) {
+            snakeParts.add(new Point(head.x, head.y));
+        }
         timer.start();
+
 
     }
 
@@ -49,26 +57,29 @@ public class Snake implements ActionListener {
         if (ticks % 10 == 0 && head != null && over == false){
             snakeParts.add(new Point(head.x, head.y));
             if (direction == UP) {
-                if (head.y - 1 > 0 )
-                    snakeParts.add(new Point(head.x,head.y-1));
+                if (head.y - 1 >= 0 )
+                    head = new Point(head.x,head.y-1);
                 else over = true;}
             if (direction == DOWN)
                 if (head.y + 1 < dim.height / SCALE )
-                snakeParts.add(new Point(head.x,head.y + 1));
+                head = new Point(head.x,head.y + 1);
                 else over = true;
             if (direction == LEFT)
-                if (head.x - 1 > 0 )
-                    snakeParts.add(new Point(head.x-1,head.y));
+                if (head.x - 1 >= 0 )
+                    head = new Point(head.x-1,head.y);
                 else over = true;
             if (direction == RIGHT)
                 if (head.x + 1 < dim.width / SCALE )
-                    snakeParts.add(new Point(head.x+1,head.y));
-                else over = true;
-            head = snakeParts.get(snakeParts.size()-1);
-            snakeParts.remove(0);
+                    head = new Point(head.x+1,head.y);
+                else
+                    over = true;
+ /*               snakeParts.add(new Point(head.x, head.y));
+            for ( int i =0; i < tailLength; i++) {
+                snakeParts.remove(0);
+            }*/
             if (cherry != null){
                 if (head.equals(cherry)){
-                    score+=10;
+                    score += 10;
                     tailLength++;
                     cherry.setLocation(dim.width / SCALE, dim.height / SCALE);
                 }
@@ -83,5 +94,28 @@ public class Snake implements ActionListener {
     }
 
 
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int i = e.getKeyCode();
+        if(i == KeyEvent.VK_A && direction != RIGHT)
+            direction = LEFT;
+        if(i == KeyEvent.VK_D && direction != LEFT)
+            direction = RIGHT;
+        if(i == KeyEvent.VK_W && direction != DOWN)
+            direction = UP;
+        if(i == KeyEvent.VK_S && direction != UP)
+            direction = DOWN;
+        if (i == KeyEvent.VK_SPACE && over)
+            snake = new Snake();
+    }
 
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
 }
